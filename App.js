1122a0767deca9e5746990e00ecfe186f4e450e2
nichdevcode <duadeinicholas14@gmail.com>
2023-09-03 20:45:36 +0100

    react native 101

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is so nice</Text>
+      <Text>I had no idea it'd refresh like this</Text>
+      <Text>Alright. React Native, here we gtghyhyhyhyhyugrtgbtgbhh  ugrfugfrjkgfrjkgfrjhfjkfr</Text>
       <StatusBar style="auto" />
     </View>
   );
